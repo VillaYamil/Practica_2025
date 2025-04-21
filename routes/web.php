0128ec49bd3e9afde6b922160f5029e5592ac60a
Villa Yamil <yamil.gchu@gmail.com>
2025-04-21 10:55:20 -0300

    crud

---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+
+Route::get('/', function () {
+    return 'Hola perreques';
+});
+
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/create',[PostController::class,'create']);
+Route::get('/posts/{post}',[PostController::class,'show']);
+
+Route::get('prueba', function(){
+    //return "Hola desde la ruta prueba";
+
+    /*$post = new Post;
+
+    $post->title = "Titulo de prueba 1";
+    $post->content = "Conternido de prueba 1";
+    $post->categoria = "Categoria de prueba 1";
+    
+    
+    //buscar y modificar por ID - Find
+    $post = Post::find(3);
+
+    //buscar y modificar por otros atributos - Where
+    //$post = Post::where('id','3')->first(); - first devuelve el primer registro que encuentre con esos valores
+
+    $post -> title = "Titulo de prueba 3";
+    $post -> content = "Conternido de prueba 3";
+    $post -> categoria = "Categoria de prueba 3";
+
+    $post->save();
+    //recuperar mas de un registro ALL
+    //$post = Post::all();
+    
+    //recuperar registros que cumplan condiciones
+    //$post = Post::where('id','>=','2')->get(); //trae mas de un registro
+
+    //ordenar los campos descente
+    $post = Post::orderBy('id','asc')
+        ->select('id','title','categoria') //muestra campos especificos
+        ->take(2)//limita la cantidad de resgistros que traiga (2 primeros)
+        ->get();
+
+    return $post;*/
+
+    $post = Post::find(4);
+
+    $post -> delete();
+
+    return "Eliminado correctamente";
+
+});
+
+// Route::get('/{post}/{category?}', function ($post, $category = null) {
+
+//     if ($category){
+//         return "Aca se muestra el post {$post} de la categoria {$category}";
+//     }
+
+//     return "Aca se muestra el post {$post}";
+
+// });
+
